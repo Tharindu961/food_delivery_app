3105922882f5d3dc4b95fde7defe975f0e7aa0c7
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-06-05 21:52:30 +0530

    Added FirstHalf class

--- lib/main.dart
+++ lib/main.dart
@@ -39,3 +39,16 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+class FirstHalf extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(35, 25, 0, 0),
+      child: Column(
+        children: <Widget>[
+          CutomAppBar(),,
+        ],),
+    )
+  }
+}
