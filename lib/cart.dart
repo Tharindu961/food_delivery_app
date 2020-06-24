7ea30d3ebff8ef058189181920acc9b26ccb00e6
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-06-24 23:24:04 +0530

    Added statelessWidget

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
+class Cart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
