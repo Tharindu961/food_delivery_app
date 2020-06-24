cb22b82fe72ed5a54afe106b83a7c7af27cd98e6
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-06-24 23:37:04 +0530

    Update cart.dart

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'model/fooditem.dart';
+
+import 'bloc/cartListBloc.dart';
 
 class Cart extends StatelessWidget {
+
+  final CartListBloc bloc = BlocProvider.getBloc<CartListBloc>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
 
+    List<FoodItem> foodItems;
+
+
+    return StreamBuilder(
+      stream: bloc.listStream,
+      builder: (context, snapshot) {
+        if (snapshot.data != null){
+          foodItems = snapshot.data;
+        }
+      },
     );
   }
 }
